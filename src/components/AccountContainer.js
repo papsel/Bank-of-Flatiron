774ae9587d5bc69db7db59221587b31d0301cb40
papsel <135683466+papsel@users.noreply.github.com>
2023-07-31 21:29:07 +0300

    Update AccountContainer.js

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,52 +4,21 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  const [query, setQuery] = useState("");
-  const [transactions, setTransactions] = useState([]);
-
+  const [transaction, setTransaction] = useState([])
+  const [query, setQuery] = useState("")
   useEffect(() => {
-    // Fetch initial transactions from the server when the component mounts
-    fetch("/api/transactions")
+    fetch("http://localhost:3000/transactions?q=" + query)
       .then((resp) => resp.json())
-      .then((data) => setTransactions(data.transactions))
-      .catch((error) => {
-        console.error("Error fetching transactions:", error);
-      });
-  }, []);
-
+      .then(transaction => setTransaction(transaction))
+  }, [query])
   function handleSearch(e) {
-    setQuery(e.target.value);
+    setQuery(e.target.value)
   }
-
-  const filteredTransactions = transactions.filter((t) =>
-    t.description.toLowerCase().includes(query.toLowerCase())
-  );
-
-  function handleAddTransaction(newTransaction) {
-    // Send the new transaction to the server to be added to db.json
-    fetch("/api/addTransaction", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTransaction),
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        // Update the local state with the new transaction received from the server
-        setTransactions((prevTransactions) => [...prevTransactions, data]);
-        alert("Added successfully!");
-      })
-      .catch((error) => {
-        console.error("Error adding transaction:", error);
-      });
-  }
-
   return (
     <div>
       <Search handleSearch={handleSearch} />
-      <AddTransactionForm onAddTransaction={handleAddTransaction} />
-      <TransactionList transactions={filteredTransactions} />
+      <AddTransactionForm />
+      <TransactionList transactions={transaction} />
     </div>
   );
 }
