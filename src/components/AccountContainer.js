9c2875219b2c8a57021b4d1e5f543169430cc507
papsel <135683466+papsel@users.noreply.github.com>
2023-07-31 20:11:47 +0300

    Update AccountContainer.js

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TransactionList from "./TransactionList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import transactions from "./Transactions";
 
 function AccountContainer() {
-  const [transaction, setTransaction] = useState([])
-  const [query, setQuery] = useState("")
-  useEffect(() => {
-    fetch("/https://api.npoint.io/cfc9529938d44733fb47/transactions?q=" + query)
-      .then((resp) => resp.json())
-      .then(transaction => setTransaction(transaction))
-  }, [query])
+  const [transaction, setTransaction] = useState(transactions);
+  const [query, setQuery] = useState("");
+
   function handleSearch(e) {
-    setQuery(e.target.value)
+    setQuery(e.target.value);
   }
+
+  // Filter the transactions based on the query
+  const filteredTransactions = transaction.filter((t) =>
+    t.description.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div>
       <Search handleSearch={handleSearch} />
       <AddTransactionForm />
-      <TransactionList transactions={transaction} />
+      <TransactionList transactions={filteredTransactions} />
     </div>
   );
 }
